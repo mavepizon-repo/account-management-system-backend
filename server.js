461944586c54224d3ad62ebf3633c768f7129c4a
Ajitha K <ajithaimmanuel@gmail.com>
2026-04-23 23:24:47 +0530

    changes done

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use("/api/labours", labourRoutes);
 
 app.use("/api/attendance", attendanceRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
